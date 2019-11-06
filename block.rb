36e6e3c802015c939a78a0a33911cfc408b9caca
SyedAbutalib <syedthegreatabutalib@gmail.com>
2019-11-06 15:25:47 -0600

    block class exists

---
+++ block.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'digest'
+require 'pp'
+
+class Block
+  attr_reader :data
+  attr_reader :hash
+
+  def initialize(data)
+    @data = data
+    @hash = Digest::SHA256.hexdigest(data)
+  end
+end
+
+pp Block.new('Hello, Cryptos!')
+
+pp Block.new('Hello Hello Hello')
