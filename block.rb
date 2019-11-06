bc62aa8109fb6b59acd8e791a546263cf6807f1d
SyedAbutalib <syedthegreatabutalib@gmail.com>
2019-11-06 16:00:43 -0600

    added proof of work

--- block.rb
+++ block.rb
@@ -6,10 +6,21 @@ require 'pp'
 class Block
   attr_reader :data
   attr_reader :hash
+  attr_reader :nonce
 
   def initialize(data)
     @data = data
-    @hash = Digest::SHA256.hexdigest(data)
+    @nonce, @hash = compute_hash_with_proof_of_work
+  end
+
+  def compute_hash_with_proof_of_work(difficulty = '00000')
+    nonce = 0
+    loop do
+      hash = Digest::SHA256.hexdigest("#{nonce}#{data}")
+      return [nonce, hash] if hash.start_with?(difficulty)
+
+      nonce += 1
+    end
   end
 end
 
